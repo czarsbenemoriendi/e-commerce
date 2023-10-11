7b4c4f57af0b6f336a55a883390a97d42f5a191a
p4w3l <93704910+czarsbenemoriendi@users.noreply.github.com>
2023-10-12 00:37:13 +0200

    feat: create error page

--- src/error.vue
+++ src/error.vue
@@ -9,7 +9,7 @@ const { error } = defineProps<Props>()
 const message = computed(() =>
   error?.message && !error.message.startsWith('Page not found')
     ? error.message
-    : 'Coś poszło nie tak',
+    : 'Something went wrong',
 )
 
 const handleGoHome = async () => {
@@ -19,8 +19,8 @@ const handleGoHome = async () => {
 <template>
   <main class="flex-1 grid place-items-center h-screen w-screen">
     <div class="flex flex-col items-center gap-8">
-      <h3 class="text-lg" data-id="error-message">{{ message }}</h3>
-      <button @click="handleGoHome">Wróć do strony głównej</button>
+      <h3 class="text-lg">{{ message }}</h3>
+      <button data-id="error-message" @click="handleGoHome">Go back home</button>
     </div>
   </main>
 </template>
