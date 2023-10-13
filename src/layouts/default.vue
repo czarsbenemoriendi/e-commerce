4fecc4d0f2300c0b049fe9b85bae866714f5cb62
p4w3l <93704910+czarsbenemoriendi@users.noreply.github.com>
2023-10-13 18:34:47 +0200

    feat: basic navigation

---
+++ src/layouts/default.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import TopBar from '@components/layout/TopBar.vue'
+</script>
+
+<template>
+  <div class="flex flex-col h-screen w-screen">
+    <TopBar class="w-11/12 self-center" />
+    <main class="flex-1 grid place-items-center p-2 overflow-auto">
+      <slot />
+    </main>
+  </div>
+</template>
