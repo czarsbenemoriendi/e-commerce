c7f9ddf982e5ea1775392b8c8fa6c4b7b4731623
p4w3l <93704910+czarsbenemoriendi@users.noreply.github.com>
2023-12-14 18:54:43 +0100

    build: remove cart icon

--- src/config/icons.ts
+++ src/config/icons.ts
@@ -2,10 +2,9 @@
 
 export const ICONS = {
   menu: () => import('@/assets/icons/Menu.svg'),
-  cart: () => import('@/assets/icons/Cart.svg'),
   likedItems: () => import('@/assets/icons/LikedItems.svg'),
   myProfile: () => import('@/assets/icons/MyProfile.svg'),
-  randomItem: () => import('@/assets/icons/RandomItem.svg')
+  randomItem: () => import('@/assets/icons/RandomItem.svg'),
 } as const
 
 export type AvailableIcon = keyof typeof ICONS
