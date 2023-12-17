1a2756687a09e1c1ef0e86be32f184024fa5d231
p4w3l <93704910+czarsbenemoriendi@users.noreply.github.com>
2023-12-17 17:10:54 +0100

    ui: add new icons

--- src/config/icons.ts
+++ src/config/icons.ts
@@ -1,8 +1,10 @@
 // get new icons from https://icones.js.org/collection/fluent
 
 export const ICONS = {
-  menu: () => import('@/assets/icons/Menu.svg'),
+  clock: () => import('@/assets/icons/Clock.svg'),
   likedItems: () => import('@/assets/icons/LikedItems.svg'),
+  meat: () => import('@/assets/icons/Meat.svg'),
+  menu: () => import('@/assets/icons/Menu.svg'),
   myProfile: () => import('@/assets/icons/MyProfile.svg'),
   randomItem: () => import('@/assets/icons/RandomItem.svg'),
 } as const
