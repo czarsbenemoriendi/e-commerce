e7c4718287fefc789620fea41c36769a83176823
p4w3l <93704910+czarsbenemoriendi@users.noreply.github.com>
2023-12-17 17:14:39 +0100

    build: create index for blocks components

---
+++ src/blocks/index.ts
@@ -0,0 +1,6 @@
+import BottomNavigation from './BottomNavigation.vue'
+import MealCard from './MealCard.vue'
+import Sidebar from './Sidebar.vue'
+import TopBar from './TopBar.vue'
+
+export { BottomNavigation, TopBar, Sidebar, MealCard }
