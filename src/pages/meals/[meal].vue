022c96b0b904c3b9b986dbed3d92010726b71491
p4w3l <93704910+czarsbenemoriendi@users.noreply.github.com>
2024-01-01 15:01:24 +0100

    build: introduce meal page

---
+++ src/pages/meals/[meal].vue
@@ -0,0 +1,20 @@
+<script setup lang="ts"></script>
+<template>
+  <div data-id="meal-page">
+    <header>
+      <div class="rounded-md">
+        <img
+          src="/meal-img/spaghetti.jpg"
+          alt=""
+          class="object-cover object-center w-full rounded-md h-52"
+        />
+      </div>
+      <div class="flex items-center justify-between pt-6 pb-2">
+        <h2 class="text-lg font-semibold">Nam maximus purus</h2>
+        <a class="block text-xs underline text-orange-600 font-Roboto h-full"
+          >See the recipe</a
+        >
+      </div>
+    </header>
+  </div>
+</template>
