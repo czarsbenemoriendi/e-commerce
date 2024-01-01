c7e0a2d170a1bd10e9906094c69ed839886518d6
p4w3l <93704910+czarsbenemoriendi@users.noreply.github.com>
2024-01-01 14:46:33 +0100

    ui: introduce new icon

--- src/config/icons.ts
+++ src/config/icons.ts
@@ -5,8 +5,10 @@ export const ICONS = {
   likedItems: () => import('@/assets/icons/LikedItems.svg'),
   meat: () => import('@/assets/icons/Meat.svg'),
   menu: () => import('@/assets/icons/Menu.svg'),
+  dashBoard: () => import('@/assets/icons/Menu.svg'),
   myProfile: () => import('@/assets/icons/MyProfile.svg'),
   randomItem: () => import('@/assets/icons/RandomItem.svg'),
+  rightArrow: () => import('@/assets/icons/RightArrow.svg'),
 } as const
 
 export type AvailableIcon = keyof typeof ICONS
